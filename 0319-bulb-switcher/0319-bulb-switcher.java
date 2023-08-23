71c70d95f9f8ada0b2b19176bf2b8bedaeb8e293
ROHIT17031 <112487841+ROHIT17031@users.noreply.github.com>
2023-08-23 11:11:09 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (27.50%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.java
+++ 0319-bulb-switcher/0319-bulb-switcher.java
@@ -1,7 +1,7 @@
 class Solution {
     public int bulbSwitch(int n) {
          int ans = 0;
-        for(int i = 0; i*i<=n; i++){
+        for(int i = 0; i*i<=n; i++){ //tc o(1)
             ans = i;
         }
         return ans;
