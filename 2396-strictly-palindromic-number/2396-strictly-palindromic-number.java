086a525be4c0f6c0bd84564c214d58fe1e69d471
ROHIT17031 <112487841+ROHIT17031@users.noreply.github.com>
2023-08-25 11:14:57 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (58.12%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
@@ -1,28 +1,29 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-               int b = 2;
-   StringBuilder sb = new StringBuilder();
-   for(int i =b; i<=n-2; i++){
+//                int b = 2;
+//    StringBuilder sb = new StringBuilder();
+//    for(int i =b; i<=n-2; i++){
     
-    int t = n;
-    while(t>0){
-      sb.append(t%i);
-      t/=b;
-    }
+//     int t = n;
+//     while(t>0){
+//       sb.append(t%i);
+//       t/=b;
+//     }                                   //100% faster
   
-    int s =0;
-    int l = sb.length()-1;
+//     int s =0;
+//     int l = sb.length()-1;
    
-    while(s<l){
-      if(sb.charAt(s)!=sb.charAt(l)){
-          return false;
-      }
-      s++;
-      l--;
-    }
-    sb.delete(0, sb.length());
+//     while(s<l){
+//       if(sb.charAt(s)!=sb.charAt(l)){
+//           return false;
+//       }
+//       s++;
+//       l--;
+//     }
+//     sb.delete(0, sb.length());
 
-   }
-   return true;
+//    }
+//    return true;
+        return false;
     }
 }
