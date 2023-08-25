5ac6a9ebe21090d5ae54d2be0aa879638c5b3394
ROHIT17031 <112487841+ROHIT17031@users.noreply.github.com>
2023-08-25 15:43:18 +0530

    Time: 51 ms (95.62%), Space: 65.8 MB (53.08%) - LeetHub

--- 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.java
+++ 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.java
@@ -1,32 +1,48 @@
 class Solution {
     public int countDistinctIntegers(int[] nums) {
-            int n = nums.length;
-   int arr [] = new int[n*2];
-   Set<Integer> set = new HashSet<>();
-   int j = 0;
-   for(int i = 0; i<arr.length; i++){
-    if(i<nums.length){
-      arr[i] = nums[i];
-    }
-    else{
-      int num = nums[j];
-      int rvr = 0;
-      while(num>0){
-        int rem = num%10;
-        rvr = (rvr*10)+rem;
-        num /=10;
+//             int n = nums.length;
+//    int arr [] = new int[n*2];
+//    Set<Integer> set = new HashSet<>();
+//    int j = 0;
+//    for(int i = 0; i<arr.length; i++){
+//     if(i<nums.length){
+//       arr[i] = nums[i];
+//     }
+//     else{
+//       int num = nums[j];
+//       int rvr = 0;
+//       while(num>0){
+//         int rem = num%10;
+//         rvr = (rvr*10)+rem;
+//         num /=10;
+
+//       }
+//       arr[i] = rvr;
+//       j++;
+
+
+//     }
+//    }
+//    for(int i = 0; i<arr.length; i++){
+//     set.add(arr[i]);
+//    }
+
+//    return set.size();
+        
+          HashSet<Integer> set = new HashSet<>();
+    for(int i = 0; i<nums.length; i++){
+      set.add(nums[i]);
+      int RvrN = 0;
+      while(nums[i]>0){
+        int rem = nums[i]%10;
+        RvrN = (RvrN*10)+rem;
+        nums[i]/=10;
 
       }
-      arr[i] = rvr;
-      j++;
-
-
+      set.add(RvrN);
     }
-   }
-   for(int i = 0; i<arr.length; i++){
-    set.add(arr[i]);
-   }
-
-   return set.size();
+    return set.size();
+        
+        
     }
 }
