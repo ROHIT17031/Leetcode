ed0e7234909a9de0aff85b1a8a8a4d68ff7163c1
ROHIT17031 <112487841+ROHIT17031@users.noreply.github.com>
2023-08-25 12:19:48 +0530

    Time: 1 ms (54.00%), Space: 39.1 MB (84.50%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.java
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.java
@@ -1,14 +1,21 @@
 class Solution {
     public int minOperations(int n) {
-            int arr [] = new int[n];
+    //         int arr [] = new int[n];
+    // for(int i = 0; i<n; i++){
+    //   arr[i] = (2*i)+1;
+    // }
+    //  int mid = n/2;
+    //  int minop = 0;
+    // for(int i = 0; i<arr.length; i++){
+    //     int rem = (Math.abs(arr[mid]-arr[i]));
+    //       minop += rem/2;
+    // }
+    // return minop;
+          int mid = n/2;
+   int minop = 0;
     for(int i = 0; i<n; i++){
-      arr[i] = (2*i)+1;
-    }
-     int mid = n/2;
-     int minop = 0;
-    for(int i = 0; i<arr.length; i++){
-        int rem = (Math.abs(arr[mid]-arr[i]));
-          minop += rem/2;
+    
+     minop += (Math.abs(mid-i));
     }
     return minop;
     }
