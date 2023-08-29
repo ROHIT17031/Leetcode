2e2396b06d67db86d2a98057ca258d78bf626391
ROHIT17031 <112487841+ROHIT17031@users.noreply.github.com>
2023-08-29 10:02:55 +0530

    Time: 0 ms (100.00%), Space: 56.2 MB (29.38%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,36 +1,27 @@
 class Solution {
-    public void rotate(int[] nums, int k) {
+    public static void rotate(int[] nums, int k) {
+     k = k%nums.length;
+    reverse(nums, 0, nums.length-k-1);
+    reverse(nums, nums.length-k, nums.length-1);
+    reverse(nums, 0, nums.length-1);
 
-       k = k%nums.length;
-        int s = 0;
-        int l = nums.length-k-1;
-        while(s<l){
-            int t = nums[s];
-            nums[s] = nums[l];
-            nums[l] = t;
-            s++;
-            l--;
+//     for (int i = 0; i < nums.length; i++) {
+//         System.out.print(nums[i] + " ");
+        
+//     }
+   
 
-        }
-        s = nums.length-k;
-         l = nums.length-1;
-         while (s<l) {
-            
-             int t = nums[s];
-            nums[s] = nums[l];
-            nums[l] = t;
-            s++;
-            l--;
-         }
-         s = 0;
-         l = nums.length-1;
-         while (s<l) {
-              int t = nums[s];
-            nums[s] = nums[l];
-            nums[l] = t;
-            s++;
-            l--;
-            
-         }
+ }
+ public static void reverse(int nums[], int start, int end){
+    // While start index is less than end index
+    while(start < end){
+        // Swap elements at start and end indices
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        // Increment start index and decrement end index
+        start++;
+        end--;
     }
+}
 }
