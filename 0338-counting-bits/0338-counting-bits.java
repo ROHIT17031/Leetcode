d776919355ee2c35d19e635a1587530671b3a1db
ROHIT17031 <112487841+ROHIT17031@users.noreply.github.com>
2023-09-01 23:51:06 +0530

    Time: 2 ms (73.73%), Space: 46.8 MB (17.13%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,21 +1,27 @@
 class Solution {
     public int[] countBits(int n) {
-             int ans [] = new int[n+1];
-        int k = ans.length-1;
-        while(n>=0){
-            int count = 0;
-            int t = n;
-            while(t>0){
-                int d = t%2;
-                if(d==1){
-                    count++;
-                }
-                t /=2;
-            }
+//              int ans [] = new int[n+1];
+//         int k = ans.length-1;
+//         while(n>=0){
+//             int count = 0;
+//             int t = n;
+//             while(t>0){
+//                 int d = t%2;     //tc o(nlogn)
+//                 if(d==1){
+//                     count++;
+//                 }
+//                 t /=2;
+//             }
 
-            ans[k--] = count;
-            n--;
+//             ans[k--] = count;
+//             n--;
 
+//         }
+//         return ans;
+        
+          int[] ans = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i >> 1] + (i & 1);
         }
         return ans;
     }
